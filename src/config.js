f2abe34277753db496cf31302362318a9051f449
tanseigi <96316622+tanseigi@users.noreply.github.com>
2021-12-18 23:26:27 +1100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // config.js
 module.exports = {
     github: {
-        username: 'arifszn', // Your GitHub org/user name. (Required)
+        username: 'tanseigi', // Your GitHub org/user name. (Required)
         sortBy: 'stars', // stars | updated
         limit: 8, // How many projects to display.
         exclude: {
@@ -10,16 +10,16 @@ module.exports = {
         }
     },
     social: {
-        linkedin: 'ariful-alam',
-        twitter: 'arif_swozon',
+        linkedin: 'mightbebot',
+        twitter: '',
         facebook: '',
         dribbble: '',
         behance: '',
         medium: '',
-        devto: 'arifszn',
-        website: 'https://arifszn.github.io',
+        devto: '',
+        website: 'https://tanseigi.github.io',
         phone: '',
-        email: 'arifulalamszn@gmail.com'
+        email: 'tanmaySinNegi@gmail.com'
     },
     skills: [
         'PHP',
@@ -37,56 +37,56 @@ module.exports = {
     ],
     experiences: [
         { 
-            company: 'Monstarlab Bangladesh',
-            position: 'Backend Engineer II',
+            company: 'Nella Operations',
+            position: 'Jr. System Administrator',
             from: 'September 2021',
             to: 'Present'
         },
         { 
-            company: 'Orangetoolz',
-            position: 'Jr. Full Stack Engineer',
-            from: 'July 2019',
+            company: 'CSIRO (AU)',
+            position: 'ML Research Assistant',
+            from: 'August 2020',
             to: 'August 2021'
         },
         { 
-            company: 'Techvillage',
-            position: 'Jr. Software Engineer',
-            from: 'January 2019',
-            to: ' June 2019'
+            company: 'RoboGals [ANU]',
+            position: 'Team Coordinator',
+            from: 'July 2019',
+            to: ' December 2020'
         }
     ],
     education: [
         { 
-            institution: 'American International University-Bangladesh',
-            degree: 'Bachelor of Science',
-            from: '2015',
-            to: '2019'
+            institution: 'Australian National University',
+            degree: 'Bachelor of Adv. Computing (Hon)',
+            from: '2018',
+            to: '2021'
         },
         { 
-            institution: 'Cantonment College, Jessore',
-            degree: 'Higher Secondary Certificate (HSC)',
-            from: '2012',
-            to: '2014',
+            institution: 'Manipal Institute of Higher Education',
+            degree: 'Tertiary Education in Applied Physics; Bach. of Science',
+            from: '2016',
+            to: '2018',
         },
         { 
-            institution: 'Chowgacha Shahadat Pilot High School',
+            institution: 'Doon International School',
             degree: 'Secondary School Certificate (SSC)',
-            from: '2007',
-            to: '2012'
+            from: '2015',
+            to: '2017'
         }
     ],
     blog: {
         // Display blog posts from your medium or dev.to account. (Optional)
-        source: 'dev.to', // medium | dev.to
-        username: 'arifszn',
+        source: '', // medium | dev.to
+        username: '',
         limit: 3 // How many posts to display. Max is 10.
     },
     googleAnalytics: {
         // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
-        id: 'G-WLLB5E14M6' // Please remove this and use your own tag id or keep it empty
+        id: '' // Please remove this and use your own tag id or keep it empty
     },
     hotjar: {
-        id: '2617601', //  Please remove this and use your own id or keep it empty
+        id: '', //  Please remove this and use your own id or keep it empty
         snippetVersion : 6
     },
     themeConfig: {
