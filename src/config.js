89b4a294d7bc1dc2080036fc4fb293a43c9bc90b
Tanmay Sin Negi <96316622+tanseigi@users.noreply.github.com>
2021-12-20 09:13:09 +1100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ module.exports = {
         limit: 8, // How many projects to display.
         exclude: {
             forks: false, // Forked projects will not be displayed if set to true.
-            projects: ['PersonalWebsite','tanmay.github.io','machine-learning','ML_Archives','ENGN-452'] // These projects will not be displayed. example: ['my-project1', 'my-project2']
+            projects: ['PersonalWebsite','tanmay.github.io','machine-learning','ML_Archives','ENGN-4528','Kaggle-Data-Analytics','gatsby-starter-portfolio-cara'] // These projects will not be displayed. example: ['my-project1', 'my-project2']
         }
     },
     social: {
